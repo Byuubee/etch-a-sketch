9ddc6f857383b1fd1bdf090c456d8b1f4c57a584
Vincent Ulrich T. Valenzuela <retsvalenzuela@gmail.com>
2021-02-04 15:43:25 +0800

    add color picker

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
 const gridContainer = document.getElementById('grid-container');
 const resetBtn = document.getElementById('reset-btn');
-let slider = document.getElementById("myRange");
-let output = document.getElementById("slider-value");
-
-
-slider.oninput = function setGridSize() {
-  gridContainer.innerHTML='';
-  let newSize = 65 - this.value;
-  makeGrid(newSize);
-}
-
+const slider = document.getElementById("myRange");
+const colorPicker = document.getElementById('color-picker');
 
 function defaultGrid(){
     return makeGrid(16); 
@@ -22,8 +14,19 @@ function makeGrid(size){
         let gridItem = document.createElement('div');
         gridItem.className='grid-item';
         gridContainer.appendChild(gridItem);
-        gridItem.addEventListener('mouseenter', function changeDivColor(){
+
+
+        gridItem.addEventListener('mouseenter', 
+        
+        function changeDivColor(){
             gridItem.classList.add('colored');
+            gridItem.style.setProperty('--color', colorPicker.value);
+
+            colorPicker.oninput = function(){
+            if(gridItem.classList.contains('colored')){
+                gridItem.style.setProperty('--color', this.value)
+            }
+        }
         });
     }
 }
@@ -34,5 +37,12 @@ function makeGrid(size){
 resetBtn.addEventListener('click', function(e){
     window.location.reload(true); 
 });
+
 window.addEventListener('load', defaultGrid);
+
+slider.oninput = function setGridSize() {
+    gridContainer.innerHTML='';
+    let newSize = 65 - this.value;
+    makeGrid(newSize);
+  }
 
